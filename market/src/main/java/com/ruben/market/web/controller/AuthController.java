ddc45dac3f122a9cd4793c1f101a121fa029f2bd
rubenandiapimetel <rubenandiapimentel@gmail.com>
2021-11-12 18:45:36 +0100

    palportatil

--- market/src/main/java/com/ruben/market/web/controller/AuthController.java
+++ market/src/main/java/com/ruben/market/web/controller/AuthController.java
@@ -3,9 +3,12 @@ package com.ruben.market.web.controller;
 import com.ruben.market.domain.dto.AuthenticationRequest;
 import com.ruben.market.domain.dto.AuthenticationResponse;
 import com.ruben.market.domain.service.UserDetailService;
+import com.ruben.market.web.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,14 +25,23 @@ public class AuthController {
 
     @Autowired
     private UserDetailService userDetailService;
+    @Autowired
+    private JWTUtil jwtUtil;
+
 
     @PostMapping("/authenticate")
     //Este metodo se encarga de reponder un json webn token JWT
     public ResponseEntity<AuthenticationResponse> createToken(@RequestBody AuthenticationRequest request){
 
-        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
-        UserDetails userDetails = userDetailService.loadUserByUsername(request.getUsername());
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+            UserDetails userDetails = userDetailService.loadUserByUsername(request.getUsername());
+            String jwt  =  jwtUtil.generateToken(userDetails);
+            return new ResponseEntity<>(new AuthenticationResponse(jwt),HttpStatus.OK);
+        }
+        catch (BadCredentialsException e){
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
 
-        return
     }
 }
--- market/src/main/java/com/ruben/market/web/security/SecurityConfig.java
+++ market/src/main/java/com/ruben/market/web/security/SecurityConfig.java
@@ -1,11 +1,17 @@
 package com.ruben.market.web.security;
 
 import com.ruben.market.domain.service.UserDetailService;
+import com.ruben.market.web.security.filter.JwtFilterRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /* En la funcion Configure le pasamos por parametro nuestro userdetailservice
 que es nuestra credencial.
@@ -16,15 +22,34 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailService userDetailService;
 
+    @Autowired
+    private JwtFilterRequest jwtFilterRequest;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailService);
     }
 
+    //esto es para que pase x alto el swagger
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui",
                 "/swagger-resources/**", "/configuration/security",
                 "/swagger-ui.html", "/webjars/**");
+    }
+
+    //todas las peticiones que terminen en /authenticate/ las permite con esta funcion
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().antMatchers("/**/authenticate").permitAll()
+                .anyRequest().authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+            http.addFilterBefore(jwtFilterRequest, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
